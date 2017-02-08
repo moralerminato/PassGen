7014580f4636c5d287a233145a77bb05548502c7
Ethical-H4CK3R <Ethical.Hacker720@gmail.com>
2017-02-08 07:18:28 -0500

    Update PassGen.py

--- PassGen.py
+++ PassGen.py
@@ -5,44 +5,32 @@
 import os
 
 class Generator(object):
-  def __init__(self,first,last):
-    self.first = first.strip()
-    self.last  = last.strip()
+ def __init__(self,first,last):
+  self.first = first.strip()
+  self.last  = last.strip()
     
-  def Modify(self,text):
-    for num in range(999):
-     text = text.title()
-     self.Write('{}{}'.format(text,num))    
-     text = text.upper()
-     self.Write('{}{}'.format(text,num))
-     text = text.lower()
-     self.Write('{}{}'.format(text,num))      
+ def Modify(self,text):
+  for num in range(999):
+   self.Write('{}{}'.format(text.title(),num))    
+   self.Write('{}{}'.format(text.upper(),num))
+   self.Write('{}{}'.format(text.lower(),num))      
  
-  def Generator(self):
-    #if len(self.first)+1 >= 8:
-    self.Modify(self.first)
-    #else: 
-     #print 'First name is less than 8 chars'
+ def Generator(self):
+  self.Modify(self.first)
+  self.Modify(self.last)
+  self.Modify('{}{}'.format(self.first,self.last))
+  self.Modify('{}{}'.format(self.last,self.first)) 
 
-    #if len(self.last)+1 >= 8:
-    self.Modify(self.last)
-    #else: 
-     #print 'Last name is less than 8 chars'
-
-    #if len(self.first) + len(self.last)+1 >= 8:
-    self.Modify('{}{}'.format(self.first,self.last))
-    self.Modify('{}{}'.format(self.last,self.first)) 
-
-    first = self.first.title()
-    last  = self.last.title()
-    for num in range(999):
-     self.Write('{}{}{}'.format(first,last,num))
-     self.Write('{}{}{}'.format(last,first,num))  
+  first = self.first.title()
+  last  = self.last.title()
+  for num in range(999):
+   self.Write('{}{}{}'.format(first,last,num))
+   self.Write('{}{}{}'.format(last,first,num))  
       
-  def Write(self,text):
-    word = '{}\n'.format(text)
-    with open('Pass.lst','a') as WriteFile:
-     WriteFile.write(word)
+ def Write(self,text):
+  word = '{}\n'.format(text)
+  with open('Pass.lst','a') as WriteFile:
+   WriteFile.write(word)
 
 if __name__ == '__main__':
  items = [item for item in os.listdir('.')]
