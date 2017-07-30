a7fa1ca8c9e15d8c72f485bd9f38dfb81f61d916
Ethical-H4CK3R <Ethical.Hacker720@gmail.com>
2017-07-30 13:54:45 -0400

    updated a few things

--- passGen.py
+++ passGen.py
@@ -10,7 +10,7 @@ import urllib2
 import subprocess
 
 # current version
-__version__ = 0.1
+__version__ = 0.2
 
 class Update(object):
  def __init__(self):
@@ -66,20 +66,15 @@ class Update(object):
 
 class Generator(object):
  def __init__(self):
+  self.name  = None
   self.list  = [] # holds numbers, don't mix with numbers or symbols
   self.word  = [] # holds words, mix with numbers and symbols
   self.file  = [] # the passwords to write
   self.symb  = ['!@#','!','@','#','!!!']
-  self.l33t  = {'a':4,'b':8,'e':3,'g':9,'i':1,'l':1,'o':0,'s':5,'t':7,'z':2}
   self.nums  = [123,1234,143,1,2,3,69,6969,420,111,123456,321,12,23,24,34,2468]
 
  def now(self):
   return time.strftime('%m-%d-%Y_%I:%M:%S',time.localtime())
-
- def leet(self,word):
-  locations = [ndex for ndex,letter in enumerate(word) if letter.lower() in self.l33t]
-  leets = [''.join([str(self.l33t[letter.lower()]) if letter.lower() in self.l33t and ndex == num else letter for ndex,letter in enumerate(word)]) for num in locations]
-  return [case for _leet in leets for case in self.cases(_leet)]
 
  def cases(self,word):
   # firstname, Firstname, FIRSTNAME
@@ -102,47 +97,61 @@ class Generator(object):
   return '{}{}{}'.format(essid[:-2],''.join([k for i,k in enumerate(bssid) if any([i==9,i==10,i==12,i==13])]),essid[-2:])
 
  def writefile(self):
-  with open('wordlist-{}.lst'.format(self.now()),'w') as fwrite:
+  self.name = 'wordlist-{}.lst'.format(self.now())
+  with open(self.name,'w') as fwrite:
    for item in self.file:
     fwrite.write('{}\n'.format(item))
 
  def generate(self):
-  print 'please wait ...'
-  self.file = self.list if self.list else self.file
+  print 'Generating ...'
+  [self.file.append(_) for _ in self.list if _]
   for word in self.word:
    # generate passwords
-   leet = self.leet(word)
    self.file.append(word)
-   self.file = self.file + [_leet for _leet in leet] # leet style
-   self.file = self.file + list(set([num for _leet in leet for num in self.numbers(_leet)])) # leet with number
-   self.file = self.file + list(set([sym for _leet in leet for sym in self.symbols(_leet)])) # leet with symbols
-   self.file = self.file + list(set([comb for _leet in leet for comb in self.comb(_leet)])) # leet with num & symbols
-   self.file = self.file + [num for num in self.numbers(word) if not num in self.file] # comb between word & numbers
-   self.file = self.file + [sym for sym in self.symbols(word) if not sym in self.file] # comb between word & symbols
-   self.file = self.file + [comb for comb in self.comb(word) if not comb in self.file] # comb between word, numbers, & symbols
+   self.file = self.file + [num for num in self.numbers(word) if not num in self.file] # words & numbers
+   self.file = self.file + [sym for sym in self.symbols(word) if not sym in self.file] # words & symbols
+   self.file = self.file + [comb for comb in self.comb(word) if not comb in self.file] # words, numbers, & symbols
   self.writefile()
 
 class Questions(object):
  def __init__(self):
   self.vends = ['TG1','DVW','DG8','U10','TC8']
   self.questions = {
-               0:{'name':'spousename','value':None},
-               1:{'name':'middlename','value':None},
-               2:{'name':'childname','value':None},
-               3:{'name':'firstname','value':None},
-               4:{'name':'yearOfBirth','value':None},
-               5:{'name':'spouseYOB','value':None},
-               6:{'name':'maidename','value':None},
-               7:{'name':'lastname','value':None},
-               8:{'name':'nickname','value':None},
-               9:{'name':'childYearOB','value':None},
-               10:{'name':'surname','value':None},
-               11:{'name':'petname','value':None},
-               12:{'name':'phone','value':None},
-               13:{'name':'essid','value':None},
-               14:{'name':'bssid','value':None},
-               15:{'name':'ssn','value':None},
-               16:{'name':'pin','value':None}
+
+            # target info
+            'firstname':None,
+            'middlename':None,
+            'lastname':None,
+            'nickname':None,
+            'yearOfBirth':None,
+
+            # child info
+            'childFN':None,
+            'childMN':None,
+            'childLN':None,
+            'childYOB':None,
+            'childNN':None,
+
+            # spouse info
+            'spouseFN':None,
+            'spouseMN':None,
+            'spouseLN':None,
+            'spouseYOB':None,
+            'spouseNN':None,
+
+            # pet info
+            'petname':None,
+            'petYOB':None,
+
+            # numbers
+            'ssn':None,
+            'pin':None,
+            'phone':None,
+
+            # wifi info
+            'essid':None,
+            'bssid':None
+
               }
 
  def cmds(self):
@@ -155,14 +164,12 @@ class Questions(object):
   print 'update            \tcheck for update'
   print 'generate          \tgenerate password list'
   print 'current version   \t{}'.format(__version__)
-  print 'reset [fieldname] \tclear field'
   raw_input('\n[Press Enter To Continue]')
 
  def mklist(self):
-  for num in self.questions:
-   q = self.questions[num]
-   n = q['name']
-   v = q['value']
+  for q in self.questions:
+   v = self.questions[q]
+   n = q
 
    if v:
     # are we looking at a pin
@@ -174,10 +181,10 @@ class Questions(object):
     elif n == 'essid' or n == 'bssid':
       if n != 'essid':continue
       essid = v
-      bssid = [key for key in self.questions if self.questions[key]['name']=='bssid']
+      bssid = [key for key in self.questions if key == 'bssid']
       bssid = bssid[0] if bssid else bssid
       if not bssid:continue
-      bssid = self.questions[bssid]['value']
+      bssid = self.questions[bssid]
 
       # generate default password
       if len(essid)>3:
@@ -188,25 +195,26 @@ class Questions(object):
 
     # full name
     elif n == 'firstname':
-     firstname,lastname = v,[self.questions[key]['value'] for key in self.questions if self.questions[key]['name']=='lastname'][0]
+     firstname,lastname = v,self.questions['lastname']
      generator.word.append(firstname)
 
      if lastname:
       generator.word.append('{}{}'.format(firstname,lastname.lower()))
       generator.word.append('{}{}'.format(firstname,lastname))
 
-    elif n == 'spousename' or n == 'childname':
+    elif n == 'spouseFN' or n == 'childFN':
      # love makes people weak
      generator.word.append(v)
      generator.word.append('ilove{}'.format(v))
      generator.word.append('love{}'.format(v))
      generator.word.append('iloveU{}'.format(v))
      generator.word.append('mylove{}'.format(v))
+     generator.word.append('{}143'.format(v))
 
     # are we looking basic info
     else:
      if v.isdigit():
-      if any([n=='yearOfBirth',n=='spouseYOB',n=='childYearOB']):
+      if any([n=='yearOfBirth',n=='spouseYOB',n=='childYOB','petYOB']):
        rev  = ''.join([n for n in reversed([n for n in v])])
        end  = v[-2:]
 
@@ -231,7 +239,7 @@ class Questions(object):
 
  def fresh(self):
   # fresh start ?
-  return False if [key for key in self.questions if self.questions[key]['value']] else True
+  return False if [_  for _ in self.questions if self.questions[_]] else True
 
  def display(self):
   subprocess.call(['clear'])
@@ -240,26 +248,25 @@ class Questions(object):
     print '-------------------------------'
     print '||    Name     ||    Value   ||'
     print '-------------------------------'
-   q = self.questions[q]
-   n = str(q['name']).ljust(11)
-   v = str(q['value']).ljust(7)
+   n,v = q.ljust(11),str(self.questions[q]).ljust(7)
    print '|| {} || {}'.format(n,v)
+  passwords = len(generator.file)
+  if passwords:
+   print '\nGenerated: {}\nSize: {}'.format(generator.name,passwords)
 
  def reset(self,item=None):
   del generator.list[:]
   del generator.word[:]
-  for num in self.questions:
-   q = self.questions[num]
-   if not item:
-    q['value'] = None
-   else:
-    if q['name']==item:
-     q['value'] = None
+  for q in self.questions:
+   self.questions[q] = None
 
 def main():
  while 1:
   answers.display()
-  print '\ntype: \'help\' for help' if answers.fresh() else ''
+  if not generator.file:
+   print '\ntype: \'help\' for help' if answers.fresh() else ''
+  if generator.file:
+   del generator.file[:]
   cmd = raw_input('\n> ')
   subprocess.call(['clear'])
 
@@ -269,27 +276,19 @@ def main():
     name = cmd.split()[0].lower().strip().replace('\n','')
     value = cmd.split()[2].lower().strip().replace('\n','')
    except:continue
-   key = [key for key in answers.questions if answers.questions[key]['name'].lower() == name]
+   key = [_ for _ in answers.questions if _.lower() == name.lower()]
    if not key:continue
    key = key[0]
-
-   # check if name exists
-   if key:
-    q = answers.questions[key]
-    v = value.upper() if q['name']=='essid' or q['name']=='bssid' else value.title()
-    q['value'] = v
-   continue
+   if key != 'bssid' and key != 'essid':
+    answers.questions[key] = str(value).title()
+   else:
+    answers.questions[key] = str(value).upper()
 
   else:
    # check for key words, such as help, reset, and generate
    cmd = cmd.lower()
    [answers.cmds() if cmd=='help' else answers.reset() if cmd=='reset' else answers.mklist() if cmd=='generate' else exit() if cmd=='exit' else Update().check() if cmd=='update' else None]
 
-   # reset field
-   cmd = cmd.split()
-   if len(cmd)==2:
-    if cmd[0] == 'reset' and [key for key in answers.questions if answers.questions[key]['name'] == cmd[1]][0]:
-     answers.reset(cmd[1])
 
 if __name__ == '__main__':
  answers = Questions()
